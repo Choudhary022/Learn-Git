93610b819342c6ae298bc980d02f2d6752f127d7
Akash <chakashk@gmail.com>
2024-06-02 08:11:23 +0530

    Created interfaces First Commit

---
+++ dmeo.ts
@@ -0,0 +1,18 @@
+
+// git demo 
+
+interface College{
+
+    student:Student
+    teacher:Teacher
+}
+
+interface Student {
+    name:string
+    roll_no:string
+}
+
+interface Teacher{
+name:string
+age:number
+}
